56e1254ce765384d04bd245abcd911b756a4b103
krzychug <krzychug@icloud.com>
2023-01-23 11:21:34 +0100

    Initial Commit

---
+++ Spendings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spendings
+//
+//  Created by Krzysztof Gruszczyński on 23/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
