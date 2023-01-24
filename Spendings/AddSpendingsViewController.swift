45afa663def6020cd4e3d1d675f7be8f3032dac7
krzychug <krzychug@icloud.com>
2023-01-24 10:43:26 +0100

    24.01

--- Spendings/AddSpendingsViewController.swift
+++ Spendings/AddSpendingsViewController.swift
@@ -8,15 +8,50 @@
 import UIKit
 
 class AddSpendingsViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
     @IBAction func cancelButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func categoryButtonChoosed(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 1:
+            categoryLabel.text = "Zakupy"
+        case 2:
+            categoryLabel.text = "Jedzenie i napoje"
+        case 3:
+            categoryLabel.text = "Usługi"
+        case 4:
+            categoryLabel.text = "Podróże"
+        case 5:
+            categoryLabel.text = "Transport"
+        case 6:
+            categoryLabel.text = "Rozrywka"
+        case 7:
+            categoryLabel.text = "Zdrowie"
+        case 8:
+            categoryLabel.text = "Dom"
+        case 9:
+            categoryLabel.text = "Cykliczne"
+        default:
+            return
+        }
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
 
